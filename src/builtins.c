440201cccd96e5464dee6537d142c413d2d8c3e3
popstarmav <cj.johnson2017@gmail.com>
2024-09-26 19:40:06 -0500

    repo update

--- src/builtins.c
+++ src/builtins.c
@@ -1,50 +1,25 @@
 #include <shell.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-int shell_cd(char **args)
+void exit_shell()
 {
-    if (args[1] == NULL) 
-    {
-        if (chdir(getenv("HOME")) != 0) {
-            perror("shell");
-        }
-    } else {
-        if (chdir(args[1]) != 0) {
-            perror("shell");
-        }
-    }
-    return 1;
+	exit(0)
 }
 
-int shell_exit(char **args)
+void change_directory(char *command)
 {
-    return 0;
+	char *directory = extract_directory_from_command(command);
+
+	if (chdir(directory) != 0)
+	{
+		perror("cd error");
+	}
 }
 
-int shell_env(char **args)
+void (print_environment)
 {
-    extern char **environ;
-    for (int i = 0; environ[i] != NULL; i++) {
-        printf("%s\n", environ[i]);
-    }
-    return 1;
-}
-
-char *builtin_str[] = {
-    "cd",
-    "exit",
-    "env"
-};
-
-int (*builtin_func[]) (char **) = {
-    &shell_cd,
-    &shell_exit,
-    &shell_env
-};
-
-int shell_num_builtins()
-{
-    return sizeof(builtin_str) / sizeof(char *);
-}
+	extern char **environ;
+	for (int i = 0; environ[i] != NULL; i++)
+	{
+		printf("%s\n", environ[i]);
+	}
+}			
