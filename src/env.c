f015a71912a089ae976fbbe9e79696dcb1ee6466
popstarmav <cj.johnson2017@gmail.com>
2024-09-29 13:23:51 -0500

    make file

--- src/env.c
+++ src/env.c
@@ -1,7 +1,8 @@
+#include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_enviroment();
+void print_enviroment()
 {
         extern char **environ;
         for (int i = 0; environ[i] != NULL; i++)
