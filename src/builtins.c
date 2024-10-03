35822f8b30deb5988a77216136ff82d15d8cb09e
popstarmav <cj.johnson2017@gmail.com>
2024-10-03 06:37:34 -0500

    Updated change_directory function

--- src/builtins.c
+++ src/builtins.c
@@ -3,34 +3,40 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void exit_shell() {
+void exit_shell() 
+{
     exit(0);
 }
 
-int change_directory(char *command) {
-    char *home_dir;
-
-    if (command == NULL || compare_strings(command, "") == 0) {
-        home_dir = getenv("HOME");
-        if (home_dir == NULL) {
-            fprintf(stderr, "cd error: HOME environment variable not set\n");
-            return -1;
-        }
-        command = home_dir;
+int change_directory(char *command) 
+{
+	char *home_dir;
+	
+	if (command == NULL || *command == '\0') 
+    {
+	    home_dir = getenv("HOME");
+	    if (home_dir == NULL) 
+	    {
+		    fprintf(stderr, "cd error");
+		    return (-1);
+	    }
+	    command = home_dir;
     }
-
-    if (chdir(command) != 0) {
-        perror("cd error");
-        return -1;
-    }
-
-    return 0;
+	if (chdir(command) != 0)
+	{
+		perror("cd error");
+		return (-1;)
+	}
+	
+	return (0);
 }
 
-void print_environment() {
-    extern char **environ;
-    for (int i = 0; environ[i] != NULL; i++) {
-        printf("%s\n", environ[i]);
-    }
+void print_environment() 
+{
+	extern char **environ;
+	for (int i = 0; environ[i] != NULL; i++) 
+	{
+		printf("%s\n", environ[i]);
+	}
 }
 
